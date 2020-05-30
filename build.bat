a94f60cb15c475e1b69a4d8513b8a63190a067c2
Javier San Juan Cervera <jedive@gmail.com>
2020-05-30 23:16:13 +0200

    .

--- build.bat
+++ build.bat
@@ -8,5 +8,4 @@ cmake -G "MinGW Makefiles" -DCMAKE_C_FLAGS=-m32 -DCMAKE_BUILD_TYPE=MinSizeRel ..
 mingw32-make
 cd ..
 move "_CMAKE\astro_exe.exe" "_build\astro.exe"
-move "_CMAKE\libastro_lib.dll" "_build\dll\astro.dll"
 pause
