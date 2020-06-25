a7a2c73274ac71b6bc3829ae2cbb9b2406e75a71
JaviCervera <jedive@gmail.com>
2020-06-25 10:29:35 +0200

    Updated build.sh script

--- build.sh
+++ build.sh
@@ -8,5 +8,4 @@ cd _CMAKE
 cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=MinSizeRel ..
 make
 cd ..
-mv _CMAKE/astro_exe _build/runtime
-mv _CMAKE/libastro_lib.so _build/astro.so
+mv _CMAKE/astro_exe _build/astro.bin
