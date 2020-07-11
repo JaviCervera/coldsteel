de1f99edf40c04e55813cb3b8bd26a7c96a828e2
Javier San Juan Cervera <jedive@gmail.com>
2020-07-11 22:40:43 +0200

    Some preprocessor definitions moved from IrrCompileConfig.h to prepare.bat.

--- prepare.bat
+++ prepare.bat
@@ -11,7 +11,7 @@ cd ..
 
 echo # Configuring Irrlicht ...
 cd _CMAKE/_IRRLICHT
-cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=MinSizeRel -DIRRLICHT_M32=ON -DIRRLICHT_SHARED=OFF -DSDL_ENABLED=ON -DSDL_PATH=../../lib/sdl ../../lib/irrlicht
+cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=MinSizeRel -DIRRLICHT_M32=ON -DIRRLICHT_SHARED=OFF -DSDL_ENABLED=ON -DSDL_PATH=../../lib/sdl ../../lib/irrlicht -DCMAKE_CXX_FLAGS="-DNO_IRR_COMPILE_WITH_SOFTWARE_ -DNO_IRR_COMPILE_WITH_BURNINGSVIDEO_"
 cd ..
 
 echo # Configuring Astro ...
