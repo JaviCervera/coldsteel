ac8748a2880d26000c2c22911e5a7f5b46309fcd
JaviCervera <JaviCervera@users.noreply.github.com>
2021-09-03 20:35:39 +0200

    Linux support

--- build.sh
+++ build.sh
@@ -1,11 +1,31 @@
 #!/bin/sh
 cd `dirname $0`
 
-python3 process_defs.py
+echo "# Generating Lua wrapper ..."
+swig -lua -c++ -o src/lua_wrapper.cpp coldsteel.i
 
+echo "# Creating folders for Cmake ..."
 mkdir _CMAKE
-cd _CMAKE
-cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=MinSizeRel ..
+mkdir _CMAKE/_COLDSTEEL
+mkdir _CMAKE/_IRRLICHT
+
+echo "# Configuring Irrlicht ..."
+cd _CMAKE/_IRRLICHT
+cmake -G "Unix Makefiles" -DCMAKE_CXX_FLAGS="-DNO_IRR_COMPILE_WITH_SOFTWARE_ -DNO_IRR_COMPILE_WITH_BURNINGSVIDEO_" -DCMAKE_BUILD_TYPE=MinSizeRel -DIRRLICHT_SHARED=OFF ../../lib/irrlicht
+cd ../..
+
+echo "# Configuring ColdSteel ..."
+cd _CMAKE/_COLDSTEEL
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=MinSizeRel ../..
+cd ../..
+
+echo "# Compiling Irrlicht ..."
+cd _CMAKE/_IRRLICHT
 make
-cd ..
-mv _CMAKE/coldsteel_exe _build/coldsteel.bin
+cd ../..
+
+echo "# Compiling ColdSteel ..."
+cd _CMAKE/_COLDSTEEL
+make
+cd ../..
+mv _CMAKE/_COLDSTEEL/coldsteel _build/coldsteel
