c9ac97c05ee1fe73d4d2d5020f5dfd8f5cf381b3
JaviCervera <JaviCervera@users.noreply.github.com>
2022-12-11 21:07:32 +0100

    HaxeWrapperBuilder is now interpreted

--- build_haxe_wrappers.bat
+++ build_haxe_wrappers.bat
@@ -2,7 +2,5 @@
 
 mkdir _build\haxe
 swig.exe -xml -xmllite -c++ -o _build/haxe/coldsteel.xml coldsteel.i
-haxe --cpp _build/haxe/HaxeWrapperBuilder -m HaxeWrapperBuilder
-_build\haxe\HaxeWrapperBuilder\HaxeWrapperBuilder
-rem del _build\haxe\coldsteel.xml
-rem rmdir /s /q _build\haxe\HaxeWrapperBuilder
+haxe -m HaxeWrapperBuilder --interp
+del _build\haxe\coldsteel.xml
