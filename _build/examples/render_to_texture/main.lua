e5b3dfda5ae800d3069ef540415026b70c87b276
JaviCervera <JaviCervera@users.noreply.github.com>
2025-04-10 14:03:22 +0200

    Added render_to_texture example

---
+++ _build/examples/render_to_texture/main.lua
@@ -0,0 +1,33 @@
+OpenScreen(400, 400, DesktopDepth(), SCREEN_RESIZABLE)
+SetAmbient(COLOR_WHITE)
+
+local tex = CreateRenderTexture(128, 128)
+
+local cam = CreateCamera()
+SetCameraRenderTarget(cam, tex)
+SetEntityPosition(cam, 0, 0, -2)
+
+local mesh = CreateMesh()
+local surf = AddSurface(mesh)
+AddVertex(surf, 0, 0.5, 0, 0, 0, -1, COLOR_CYAN, 0, 0)
+AddVertex(surf, 0.5, -0.5, 0, 0, 0, -1, COLOR_YELLOW, 0, 0)
+AddVertex(surf, -0.5, -0.5, 0, 0, 0, -1, COLOR_MAGENTA, 0, 0)
+AddIndex(surf, 1)
+AddIndex(surf, 2)
+AddIndex(surf, 3)
+UpdateMesh(mesh)
+
+local triangle = CreateModel(mesh)
+FreeMesh(mesh)
+SetMaterialCullingEnabled(EntityMaterial(triangle, 1), false)
+
+while not ScreenShouldClose() and not KeyDown(KEY_ESC) do
+    TurnEntity(triangle, 0, 64 * DeltaTime(), 0)
+
+    DrawWorld()
+    SetRenderTarget(tex, false, false, 0)
+    DrawText(nil, Str(ScreenFPS()) .. " FPS", 2, 2, COLOR_WHITE)
+    SetRenderTarget(nil, false, false, 0)
+    DrawTextureEx(tex, 0, 0, ScreenWidth(), ScreenHeight(), COLOR_WHITE)
+    RefreshScreen()
+end
