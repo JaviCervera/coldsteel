cddecc300ce36aeef9e4da55d0a16cddd86ebe04
JaviCervera <JaviCervera@users.noreply.github.com>
2025-04-18 09:57:34 +0200

    Switch Win95 compiler to TDM-GCC 5.1.0

--- build_win95.bat
+++ build_win95.bat
@@ -3,6 +3,8 @@
 echo # Building coldsteel cli
 g++ -O2 -static -s -fno-rtti -o _build/coldsteel.exe coldsteel.cpp lib/lua/*.c
 
+set PATH=%~dp0TDM-GCC-32\bin;%PATH%
+
 echo # Generating Lua wrapper ...
 swig.exe -lua -c++ -o src/lua_wrapper.cpp coldsteel.i
 
@@ -10,21 +12,16 @@ echo # Creating folders for CMake ...
 mkdir _CMAKE\_IRRLICHT
 mkdir _CMAKE\_COLDSTEEL
 
-echo # Use old version on MinGW
-set CC=%~dp0mingw_342/bin/mingw32-gcc.exe
-set CXX=%~dp0mingw_342/bin/mingw32-g++.exe
-set RC=%~dp0mingw_342/bin/windres.exe
-
 echo # Building Irrlicht (Desktop) ...
 cd _CMAKE/_IRRLICHT
-..\..\mingw_342\bin\cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=MinSizeRel -DIRRLICHT_SHARED=OFF ../../lib/irrlicht185
-..\..\mingw_342\bin\mingw32-make
+cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=MinSizeRel -DIRRLICHT_SHARED=OFF ../../lib/irrlicht185
+mingw32-make -j8
 cd ../..
 
 echo # Building csrun (Desktop) ...
 cd _CMAKE/_COLDSTEEL
-..\..\mingw_342\bin\cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=MinSizeRel -DLEGACY_IRRLICHT=ON -DCMAKE_RC_FLAGS="-O coff" ../..
-..\..\mingw_342\bin\mingw32-make
+cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=MinSizeRel -DLEGACY_IRRLICHT=ON -DCMAKE_RC_FLAGS="-O coff" ../..
+mingw32-make -j8
 move "csrun.exe" "..\..\_build\csrun.exe"
 cd ../..
 
