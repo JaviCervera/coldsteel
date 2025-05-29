cd4c53f2686afbf714d6b89f947f19bcb6a5ac83
JaviCervera <JaviCervera@users.noreply.github.com>
2025-05-29 10:11:51 +0200

    build.sh now uses 8 threads

--- build.sh
+++ build.sh
@@ -13,12 +13,12 @@ echo "# Building Irrlicht (Desktop) ..."
 cd lib/irrlicht190_ogles
 cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DIRRLICHT_SHARED=OFF -B ../../_CMAKE/_IRRLICHT
 cd ../../_CMAKE/_IRRLICHT
-make
+make -j8
 cd ../..
 
 echo "# Building coldsteel (Desktop) ..."
 cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -B _CMAKE/_COLDSTEEL
 cd _CMAKE/_COLDSTEEL
-make
+make -j8
 mv coldsteel ../../_build/coldsteel
 cd ../..
