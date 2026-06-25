b38827bb8c1f62a0556039129a6f98822d6c490e
JaviCervera <JaviCervera@users.noreply.github.com>
2026-06-25 10:38:05 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -44,14 +44,14 @@ if %errorlevel% equ 0 (
   if not exist _CMAKE\_IRRLICHT_EMSCRIPTEN mkdir _CMAKE\_IRRLICHT_EMSCRIPTEN
   if not exist _CMAKE\_COLDSTEEL_EMSCRIPTEN mkdir _CMAKE\_COLDSTEEL_EMSCRIPTEN
 
-  echo # Building Irrlicht (Emscripten) ...
+  echo # Building Irrlicht ^(Emscripten^) ...
   cd lib/irrlicht190_ogles
   call emcmake cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release -DIRRLICHT_SHARED=OFF -B ../../_CMAKE/_IRRLICHT_EMSCRIPTEN
   cd ../../_CMAKE/_IRRLICHT_EMSCRIPTEN
   call emmake make NDEBUG=1 -j8
   cd ../..
 
-  echo # Building ColdSteel (Emscripten) ...
+  echo # Building ColdSteel ^(Emscripten^) ...
   call emcmake cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release -DIRRLICHT_SVN=ON -B _CMAKE/_COLDSTEEL_EMSCRIPTEN
   cd _CMAKE/_COLDSTEEL_EMSCRIPTEN
   call emmake make NDEBUG=1 -j8
